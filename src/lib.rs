a7e399a70792a001d01bdd04fe1e3c0172dbf439
Rose Hudson <cv21874@bristol.ac.uk>
2022-02-05 00:20:44 +0000

    implement subtraction

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,21 @@
 use std::marker::PhantomData;
 
-trait Nat {}
+trait Nat {
+    type Pred: Nat;
+}
 struct Zero;
-impl Nat for Zero {}
+impl Nat for Zero {
+    type Pred = Zero;
+}
 struct Succ<T: Nat>(PhantomData<T>);
-impl<T: Nat> Nat for Succ<T> {}
+impl<T: Nat> Nat for Succ<T> {
+    type Pred = T;
+}
 
 trait IsTrue {}
+
+struct Equal<A, B>(PhantomData<(A, B)>);
+impl<N, A: Val<V = N>, B: Val<V = N>> IsTrue for Equal<A, B> {}
 
 struct GreaterThanOrEqual<A: Nat, B: Nat>(PhantomData<(A, B)>);
 impl<A: Nat> IsTrue for GreaterThanOrEqual<A, Zero> {}
@@ -16,6 +25,9 @@ impl<A: Nat, B: Nat> IsTrue for GreaterThanOrEqual<Succ<A>, Succ<B>>
 
 trait Val {
     type V: Nat;
+}
+impl<N: Nat> Val for N {
+    type V = N;
 }
 
 struct Add<A: Nat, B: Nat>(PhantomData<(A, B)>);
@@ -31,10 +43,24 @@ impl<A: Nat, B: Nat, C: Nat> Val for Add<A, Succ<B>>
     type V = Succ<C>;
 }
 
+
+struct Sub<A: Nat, B: Nat>(PhantomData<(A, B)>);
+// A - 0 = 0
+impl<A: Nat> Val for Sub<A, Zero> {
+    type V = A;
+}
+// A - B = C => A + Succ<B> = C::Pred
+impl<A: Nat, B: Nat, C: Nat> Val for Sub<A, Succ<B>>
+    where Sub<A, B>: Val<V = C>
+{
+    type V = C::Pred;
+}
+
 fn assert<T: IsTrue>() {}
 
 fn main() {
     assert::<GreaterThanOrEqual<Succ<Zero>, Zero>>();
     assert::<GreaterThanOrEqual<Zero, Zero>>();
+    assert::<Equal<Sub< Succ<Succ<Succ<Zero>>>, Succ<Succ<Zero>> >, Succ<Zero>>>();
     assert::<GreaterThanOrEqual<Zero, Succ<Zero>>>();
 }
